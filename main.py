4e75ccdbea2b1b1fd56e2817be8d1fab928c0cbe
iCr3atura <funnyrockable05@gmail.com>
2025-03-24 02:16:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def receive_webhook():
+    print("[LOG] RAW DATA:")
+    print(request.form)
+    print(request.data)
+    print(request.get_json(silent=True))
+
     data = request.form.to_dict()
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"form_{timestamp}.csv"
 
-    # Убедимся, что папка существует
     os.makedirs("data", exist_ok=True)
     filepath = os.path.join("data", filename)
 
-    # Сохраняем в CSV
     with open(filepath, mode='w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
         writer.writerow(data.keys())
         writer.writerow(data.values())
-        print(f"[LOG] Saved file: {filename}")
 
+    print(f"[LOG] Saved file: {filename}")
     return {"status": "ok", "saved": filename}, 200
+
 
 # 🔧 Эта часть делает сервер доступным извне на Render
 if __name__ == "__main__":
